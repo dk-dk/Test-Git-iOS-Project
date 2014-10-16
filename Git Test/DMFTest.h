17217c82184ce1db0c74607d27754065f4b0ca1e
Mac <Mac@Host.local>
2014-10-16 10:36:38 +0200

    added a test class

---
+++ Git Test/DMFTest.h
@@ -0,0 +1,13 @@
+//
+//  DMFTest.h
+//  Git Test
+//
+//  Created by Mac on 16/10/14.
+//  Copyright (c) 2014 Dani Mar Family. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMFTest : NSObject
+
+@end
---
+++ Git Test/DMFTest.m
@@ -0,0 +1,13 @@
+//
+//  DMFTest.m
+//  Git Test
+//
+//  Created by Mac on 16/10/14.
+//  Copyright (c) 2014 Dani Mar Family. All rights reserved.
+//
+
+#import "DMFTest.h"
+
+@implementation DMFTest
+
+@end
